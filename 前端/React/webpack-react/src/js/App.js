dcd523f40c91c202c9f30fa909c94273c1b18a4a
unknown <944951481@qq.com>
2019-03-13 13:45:46 +0800

    前端

---
+++ 前端/React/webpack-react/src/js/App.js
@@ -0,0 +1,9 @@
+import React from "react"
+class App extends React.Component{
+    render(){
+        return <h1>sdfd</h1>
+    }
+}
+
+// 暴露组件，方便引用
+export  default App
---
+++ 前端/React/菜鸟教程/03_jsfile.js
@@ -0,0 +1,4 @@
+ReactDOM.render(
+  <h1>引入外部的js文件</h1>,
+  document.getElementById('jsfile')
+);
